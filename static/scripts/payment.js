4e5bcab5319b20118dff0a00934e443145e3f657
oviedojeeple <oviedojeeple@gmail.com>
2025-02-27 22:14:05 -0500

    Update payment.js

--- static/scripts/payment.js
+++ static/scripts/payment.js
@@ -46,9 +46,4 @@ document.addEventListener("DOMContentLoaded", async function () {
         modal.style.display = "none";
       }
     }
-    window.onclick = function(event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    }
 });
